6f655e51c4545b24be615a0fd29f3791f5101e33
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2015-05-21 20:06:53 +0200

    Klasa Validacija povezana sa AbstractValidacija
    Validacija (sadrzavat ce metode za validaciju) nasljedjuje klasu
    AbstractValidacija

---
+++ RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/AbstractValidacija.java
@@ -0,0 +1,103 @@
+package ba.unsa.etf.si.app.RezervacijaZGTim6;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class AbstractValidacija extends InputVerifier implements KeyListener {
+	
+	protected JDialog popup;
+    protected Object parent;
+    private JLabel messageLabel;
+    private JLabel image;
+    private Color color;
+    //private Point point;
+    //private Dimension dimension;
+	
+    private AbstractValidacija() {
+        color = new Color(255, 224, 255);
+    }
+    
+    public AbstractValidacija(JFrame frame, Component component, String message) {
+		// TODO Auto-generated constructor stub
+    	this(component, message);
+        this.parent = frame;
+        popup = new JDialog(frame);
+        initComponents();
+	}
+    
+	private AbstractValidacija(JComponent c, String message) {
+        this();
+        c.addKeyListener(this);
+        messageLabel = new JLabel(message + " ");
+        image = new JLabel(new ImageIcon("exception_16x16.png"));
+    }
+    
+    public AbstractValidacija(JDialog parent, JComponent c, String message) {		
+        this(c, message);
+        this.parent = parent;
+        popup = new JDialog(parent);
+        initComponents();
+    }
+	
+    protected AbstractValidacija(JFrame parent, JComponent c, String message) {
+        this(c, message);
+        this.parent = parent;
+        popup = new JDialog(parent);
+        initComponents();
+    }
+
+	public AbstractValidacija(Component component, String message) {
+		this();
+	    component.addKeyListener(this);
+	    messageLabel = new JLabel(message + " ");
+	    image = new JLabel(new ImageIcon("exception_16x16.png"));
+		
+	}
+
+	//protected abstract boolean validationCriteria(JComponent c);
+	//protected abstract Boolean ValidacijaTekst(String tekst);
+    //protected abstract Boolean ValidacijaImePrezime(String imePrezime);
+    //protected abstract Boolean ValidacijaTelefon(String mail);
+    protected abstract Boolean ValidacijaJMBG(String JMBG);
+    
+    protected void initComponents() {
+        popup.getContentPane().setLayout(new FlowLayout());
+        popup.setUndecorated(true);
+        popup.getContentPane().setBackground(color);
+        popup.getContentPane().add(image);
+        popup.getContentPane().add(messageLabel);
+        popup.setFocusableWindowState(false);
+    }
+    
+    /*public boolean verify(JComponent c) {		
+        if (!validationCriteria(c)) {
+        	
+            c.setBackground(Color.RED);
+            popup.setSize(0, 0);
+            popup.setLocationRelativeTo(c);
+            point = popup.getLocation();
+            dimension = c.getSize();
+            popup.setLocation(point.x-(int)dimension.getWidth()/2, point.y+(int)dimension.getHeight()/2);
+            popup.pack();
+            popup.setVisible(true);
+            return false;
+        }
+        
+        c.setBackground(Color.WHITE);
+        return true;
+    }*/
+	
+    protected void setMessage(String message) {
+       messageLabel.setText(message);
+    }
+	
+    public void keyPressed(KeyEvent e) {
+        popup.setVisible(false);
+    }
+	
+    public void keyTyped(KeyEvent e) {}
+	
+    public void keyReleased(KeyEvent e) {}
+}
--- RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/Validacija.java
+++ RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/Validacija.java
@@ -2,9 +2,31 @@ package ba.unsa.etf.si.app.RezervacijaZGTim6;
 
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+//import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.awt.Component;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
-
-public class Validacija {
+public class Validacija extends AbstractValidacija {
+	
+	public Validacija(JFrame frame, Component component, String message) {
+	    // TODO Auto-generated constructor stub
+	    super(frame, component, message);
+    }	
+	
+	public Validacija(JFrame parent, JTextField c, String message) {
+        super(parent, c, message);
+    }
+	
+	public Validacija(JFrame parent, JComponent c, String message) {
+		super(parent, c, message);
+	}	
+   
+    public boolean validationCriteria(JComponent c, String tip) {
+		if(tip.equals("JMBG")) return ValidacijaJMBG( ((JTextField)c).getText());
+	    return ValidacijaTekst( ((JTextField)c).getText());
+	}
 	
 	public boolean textboxEmpty(JTextField t)
 	{
@@ -23,5 +45,22 @@ public class Validacija {
 		else
 			return true;
 	}
+	
+	// ToDo - Pattern za telefon promijeniti zbog '\'
+	public static final Pattern validanTelefon = Pattern.compile("(0\\d{2})\\d{3}-\\d{3}");
+	
+	/*public Boolean ValidacijaTelefon(String telefon)
+	{
+		Matcher matcher = validanTelefon.matcher(telefon);
+        return matcher.find();
+	}*/
+	
+	public Boolean ValidacijaJMBG(String jmbg) { return true; } // poslije impl
+	public Boolean ValidacijaTekst(String tekst) { return true; } // poslije impl
 
+	@Override
+	public boolean verify(JComponent input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
