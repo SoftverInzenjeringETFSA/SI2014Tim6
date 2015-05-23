e0085f4d4c81b2a2416de1e9e811a831a2b98040
Anesa Drakovac <adrakovac1@etf.unsa.ba>
2015-05-23 19:40:38 +0200

    Radnici validacija

--- RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/Forme/Radnici.java
+++ RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/Forme/Radnici.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.si.app.RezervacijaZGTim6.Forme;
 
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -24,6 +25,7 @@ import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.SystemColor;
@@ -40,6 +42,11 @@ public class Radnici {
 	private JTextField textField;
 	private JTable table;
 	private Restoran handler;
+	ImageIcon alImg = new ImageIcon("Slike/alert.png");
+	JLabel lblRadnik = new JLabel("", alImg, SwingConstants.LEFT);
+	JLabel lblObrisiIzmijeni = new JLabel("", alImg, SwingConstants.LEFT);
+	JPanel pnlRadnik = new JPanel();
+	JPanel pnlObrisiIzmijeni = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -103,6 +110,50 @@ public class Radnici {
 		textField.setColumns(10);
 		
 		JButton btnPretrazi = new JButton("Pretraži");
+		btnPretrazi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				 boolean validna_forma = true;
+	                
+	                // validacija
+	                if (textField.getText().isEmpty()) {
+	                    lblRadnik.setText("Popunite polje!");
+	                    pnlRadnik.setVisible(true);
+	                    validna_forma = false;
+	                } else if (!ValidacijaImePrezime(textField.getText())) {
+	                    lblRadnik.setText("Nedozvoljen format!");
+	                    pnlRadnik.setVisible(true);
+	                    validna_forma = false;
+	                } else if (textField.getText().equals("anesa")) {
+	                	// ovdje dodati za npr ako ne postoji radnik da izbaci error sa tekstom
+                		lblRadnik.setText("Radnik ne postoji!");
+	                    pnlRadnik.setVisible(true);
+	                    validna_forma = false;
+                	}
+	                else { 
+	                    lblRadnik.setText("");
+	                    pnlRadnik.setVisible(false);
+	                    validna_forma = true;
+	                }
+	                
+	                
+	                try {
+	                    if(validna_forma) {
+	                    	// logika za pretrazivanje radnika
+	                      	
+	                    }
+	                } catch (Exception e1) {
+	                    e1.printStackTrace();
+	                }
+	                
+	                if (validna_forma) {
+	                    System.out.println("Validna forma");
+	                } else
+	                    System.out.println("Nevalidna forma");               
+	            }
+			
+		});
+		
 		btnPretrazi.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		
 		JLabel lblNewLabel_1 = new JLabel("");
@@ -130,11 +181,19 @@ public class Radnici {
 		btnIzmjeni.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		btnIzmjeni.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				// kad se doda logika za Izmijeni i Obrisi, dodat cemo pojavu errora za sl da se ne moze to izvrsiti
 			}
 		});
 		
 		JButton btnObrisi = new JButton("Obriši");
+		btnObrisi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnObrisi.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		
+		
 		GroupLayout groupLayout = new GroupLayout(getRadnici().getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -145,29 +204,32 @@ public class Radnici {
 							.addGap(18)
 							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 								.addComponent(lblNewLabel_1)
-								.addComponent(lblNewLabel, Alignment.LEADING))
-							.addContainerGap())
+								.addComponent(lblNewLabel, Alignment.LEADING)))
 						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(45)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(btnObrisi, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
-										.addGap(9)
-										.addComponent(btnIzmjeni))
 									.addComponent(table, GroupLayout.PREFERRED_SIZE, 385, GroupLayout.PREFERRED_SIZE)
-									.addComponent(btnDodaj, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
+									.addComponent(btnDodaj, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(pnlObrisiIzmijeni, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+											.addComponent(btnObrisi, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
+											.addGap(9)
+											.addComponent(btnIzmjeni))))
 								.addGroup(groupLayout.createSequentialGroup()
 									.addComponent(lblIme)
 									.addPreferredGap(ComponentPlacement.RELATED)
 									.addComponent(textField, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
 									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btnPretrazi)))
-							.addGap(39))))
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(pnlRadnik, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
+										.addComponent(btnPretrazi))))))
+					.addGap(49))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 398, Short.MAX_VALUE)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(12)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
@@ -182,15 +244,27 @@ public class Radnici {
 								.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 								.addComponent(btnPretrazi))
 							.addGap(2)))
-					.addPreferredGap(ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(pnlRadnik, GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(table, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnIzmjeni)
 						.addComponent(btnObrisi))
-					.addGap(57))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(pnlObrisiIzmijeni, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+					.addGap(25))
 		);
+		
+		lblObrisiIzmijeni.setForeground(Color.RED);
+		pnlObrisiIzmijeni.add(lblObrisiIzmijeni);
+		pnlObrisiIzmijeni.setVisible(false);
+		
+		lblRadnik.setForeground(Color.RED);
+		pnlRadnik.add(lblRadnik);
 		panel.setLayout(null);
+		pnlRadnik.setVisible(false);
 		
 		JButton btnOdjava = new JButton("Odjavi se");
 		btnOdjava.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -247,4 +321,9 @@ public class Radnici {
 	public void setRadnici(JFrame frame) {
 		this.frame = frame;
 	}
+	
+	public Boolean ValidacijaImePrezime(String user) {
+        if (user.length() < 3) return false;
+        return user.matches("^[a-zA-Z ]+");
+    }
 }
