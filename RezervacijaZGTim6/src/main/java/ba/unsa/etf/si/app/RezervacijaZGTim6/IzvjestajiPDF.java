3bb56438fa2667e5112a7b53c6c38cd01b57b1b0
ElmirHodzic <ehodzic3@etf.unsa.ba>
2015-05-24 12:12:22 +0200

    Izvjestaji PDF

---
+++ RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/IzvjestajiPDF.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.si.app.RezervacijaZGTim6;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import javax.swing.ImageIcon;
+/*import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.view.JasperViewer;
+*/
+
+public class IzvjestajiPDF {
+	
+
+	
+	public IzvjestajiPDF() 
+	{
+	}
+	
+	public void NapraviIzvjestaj(java.util.Date od_, java.util.Date _do)
+	{
+		/*		
+		HashMap parametri = new HashMap();	
+		parametri.put("P_DATUM_OD", do_);//eventualno formatirati datum
+		parametri.put("P_DATUM_OD", _od);
+		JasperReport jr = JasperCompileManager.compileReport("izvjestaji/fajlIzvjestaja.jrxml");
+		Connection dbcon = (Connection)DriverManager.getConnection("jdbc:mysql://localhost/restoran", "root", "root");
+		JasperPrint jp = JasperFillManager.fillReport(jr, hm, dbcon);
+		JasperExportManager.exportReportToPdfFile(jp, 'C:\Nazivizvjestaja.pdf'); //dodati da se kreira jedinstven naziv
+		JasperViewer.viewReport(jp, false);
+		dbcon.close();
+		 */
+		
+	}
+	
+
+}	
