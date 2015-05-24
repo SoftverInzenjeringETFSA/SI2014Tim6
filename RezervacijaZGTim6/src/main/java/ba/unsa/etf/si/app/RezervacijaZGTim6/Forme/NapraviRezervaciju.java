cebd8fe84688d559b6e14cc047f351df9302dea9
AdnaDurakovic <adurakovic4@etf.unsa.ba>
2015-05-24 15:16:33 +0200

    Greška 2

--- RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/Forme/NapraviRezervaciju.java
+++ RezervacijaZGTim6/src/main/java/ba/unsa/etf/si/app/RezervacijaZGTim6/Forme/NapraviRezervaciju.java
@@ -155,35 +155,42 @@ public class NapraviRezervaciju {
 		JButton btnDodajRezervaciju = new JButton("Dodaj rezervaciju");
 		btnDodajRezervaciju.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				getNapraviRezervaciju().dispose();
+		
 			    
+				boolean validna_forma = true;
 				//Ovdje slijedi logika za dodavanje rezervacije u bazu podataka, te ako je uspjesna rezervacija, da putem
 				//referenci za button i panel promijenit boju stola koji je kliknut
+				if (table.getSelectedRow() == -1)
+					validna_forma= false;
 				
-				long idGosta = Long.parseLong(table.getValueAt(table.getSelectedRow(), 0).toString());
-				Time vrijeme = new Time(hours, minutes, 0);
-				Integer trajanje = (Integer)spinner.getValue();
-				Long ms = date.getDate().getTime();
-				java.sql.Date sqldate = new java.sql.Date(ms);
+				if(validna_forma)
+				{
+					System.out.println("validna forma");
+					
+					long idGosta = Long.parseLong(table.getValueAt(table.getSelectedRow(), 0).toString());
+					Time vrijeme = new Time(hours, minutes, 0);
+					Integer trajanje = (Integer)spinner.getValue();
+					Long ms = date.getDate().getTime();
+					java.sql.Date sqldate = new java.sql.Date(ms);
 				
-				try {
-					if(trajanje == 0) trajanje = 180;
-					if(idGosta != 0 && trajanje < 1440 && table.getSelectedRowCount() == 1){
-						handler.NapraviRezervaciju(idGosta, handler.getKorisnik().getID(), sto.getID(), sto.getKapacitet(), "REZERVISANO", sqldate, vrijeme, trajanje*60);
-						
-						prikazStolovaButton.setBackground(Color.red);
-						prikazStolovaPanel.revalidate();
-						prikazStolovaPanel.repaint();
+					try {
+						if(trajanje == 0) trajanje = 180;
+						if(idGosta != 0 && trajanje < 1440 && table.getSelectedRowCount() == 1){
+							handler.NapraviRezervaciju(idGosta, handler.getKorisnik().getID(), sto.getID(), sto.getKapacitet(), "REZERVISANO", sqldate, vrijeme, trajanje*60);
+							
+							prikazStolovaButton.setBackground(Color.red);
+							prikazStolovaPanel.revalidate();
+							prikazStolovaPanel.repaint();
+						}
+					} catch (NumberFormatException e1) {
+						e1.printStackTrace();
+					} catch (Exception e1) {
+						e1.printStackTrace();
 					}
-				} catch (NumberFormatException e1) {
-					e1.printStackTrace();
-				} catch (Exception e1) {
-					e1.printStackTrace();
+					getNapraviRezervaciju().dispose();
 				}
-				
-				
-				
-				
+				else
+					System.out.println("odaberite klijenta");				
 			}
 		});
 		btnDodajRezervaciju.setFont(new Font("Tahoma", Font.PLAIN, 13));
