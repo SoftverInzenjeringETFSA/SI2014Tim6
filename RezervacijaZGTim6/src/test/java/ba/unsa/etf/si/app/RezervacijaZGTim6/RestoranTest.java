4693335737f0be90cb64839c0ee193d992b5950f
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-06-01 00:17:03 +0200

    Ispravka ispravke, ne dirati hibernate

--- RezervacijaZGTim6/src/test/java/ba/unsa/etf/si/app/RezervacijaZGTim6/RestoranTest.java
+++ RezervacijaZGTim6/src/test/java/ba/unsa/etf/si/app/RezervacijaZGTim6/RestoranTest.java
@@ -1,5 +1,40 @@
 package ba.unsa.etf.si.app.RezervacijaZGTim6;
 
-public class RestoranTest {
+import junit.framework.TestCase;
 
+public class RestoranTest extends TestCase {
+
+	public void testRestoran() throws Exception
+	{
+		Restoran r= Restoran.getInstance();
+		assertNotNull(r);
+		
+	}
+	
+	public void testKorisnikNull()
+	{
+		try {
+			Restoran r= Restoran.getInstance();
+			assertNull(r.getKorisnik());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void testKorisnikGoodCredentials()
+	{
+		try {
+			Restoran r= Restoran.getInstance();
+			r.DodajKorisnika(1, "sefic", "sifra123");
+			assertEquals("sefic",r.getKorisnik().getKorisnickoIme());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
