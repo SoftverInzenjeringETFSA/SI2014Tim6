bbd1e89bdec2b09a3d89202ffc4c3e80273b1e73
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-06-01 13:19:45 +0200

    Ispravke na testovima

--- RezervacijaZGTim6/src/test/java/ba/unsa/etf/si/app/RezervacijaZGTim6/RestoranTest.java
+++ RezervacijaZGTim6/src/test/java/ba/unsa/etf/si/app/RezervacijaZGTim6/RestoranTest.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.si.app.RezervacijaZGTim6;
 
+import java.util.Date;
+
 import junit.framework.TestCase;
 
 public class RestoranTest extends TestCase {
@@ -29,6 +31,33 @@ public class RestoranTest extends TestCase {
 			Restoran r= Restoran.getInstance();
 			r.DodajKorisnika(1, "sefic", "sifra123");
 			assertEquals("sefic",r.getKorisnik().getKorisnickoIme());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void testDodavanjeGostiju()
+	{
+		try {
+			Restoran r= Restoran.getInstance();
+			r.DodajGosta("almin", "halilovic", "012345678");
+			assertTrue(r.DajGoste().contains(new Gost("almin","halilovic","012345678")));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void testDodavanjeRadnika()
+	{
+		try {
+			Restoran r = Restoran.getInstance();
+			r.DodajRadnika("almin", "halilovic", (java.sql.Date) new Date(), "2403993161712", "radnik", "radnik", (long)80);
+			assertTrue(r.DajRadnike().contains(new Radnik("almin", "halilovic", (java.sql.Date) new Date(), "2403993161712", "radnik", "radnik", (long)80)));
+			
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
